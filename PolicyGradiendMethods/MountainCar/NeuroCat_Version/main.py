308992b6ad2ac2da03c0148d006cbae213502af6
sebastia.allmeier@gmail.com <q4ev1994>
2017-12-25 21:51:16 +0100

    found errors, working on it

--- PolicyGradiendMethods/MountainCar/NeuroCat_Version/main.py
+++ PolicyGradiendMethods/MountainCar/NeuroCat_Version/main.py
@@ -7,6 +7,7 @@
 
 import gym
 import numpy as np
+import copy
 
 env = gym.make('MountainCar-v0')
 
@@ -18,34 +19,36 @@ def get_policy(theta, state, gradient=None):
     output_policy = []
     phi_values = []
     sum = 0
+    theta = theta.reshape(3)
 
     # actions:
     #       0 -- push left
     #       1 -- no push
     #       2 -- push right
 
-    for j in range(env.action_space.n):
+    for action in range(env.action_space.n):
         phi = np.zeros(shape=3)
-        a = j           # action
         p = state[0]    # position
         v = state[1]    # velocity
 
         # define features
-        if v < 0 and a == 0: # velocity is negative (going backwards) and actions is push left
+        if v < 0 and action == 0: # velocity is negative (going backwards) and actions is push left
             phi[0] = 1
-        if v > 0 and a == 2: # velocity is negative (going backwards) and actions is push right
-            phi[1] = 1
-        if p > 0 and a == 2: # position is negative (behind starting point) and actions is push right
+        if v >= 0 and action == 2: # velocity is negative (going backwards) and actions is push right
             phi[2] = 1
+        if p > 0 and action == 2:
+            phi[2] = 1 # position of the car
 
-        phi_values.append(np.reshape(phi, (3,1)))
 
-        y = np.exp(np.dot(phi, theta))
+        phi_values.append(phi)
+
+        y = np.exp(np.dot(np.transpose(phi), theta))
         sum += y
         output_policy = np.append(output_policy, y)
 
     # calculate policy outputs for all actions
     output_policy = ( 1 / sum ) * output_policy
+    phi_values = np.array(phi_values)
 
     if gradient is None:
         return output_policy
@@ -54,7 +57,7 @@ def get_policy(theta, state, gradient=None):
 
 
 def action_selection(policy):
-    x = np.random.uniform(size=1) # random number; by uniform distribution
+    x = np.random.uniform(size=1) # random number given by uniform distribution
     # ?? what is this
     if x < policy[0]:
         action = 0
@@ -78,9 +81,9 @@ def inference(theta):
 
 def learn(theta=None):
     if theta is None:
-        epsilon = 100
-        theta = np.random.uniform(-epsilon, epsilon, size=(3,1))
-    num_episodes = 20 # number of parameter updates
+        epsilon = 50
+        theta = np.random.uniform(-epsilon, epsilon, 3)
+    num_episodes = 50 # number of parameter updates
 
     for i in range(num_episodes):
         state = env.reset()
@@ -106,9 +109,9 @@ def learn(theta=None):
         # adapt parameters of policy representation
         R = 0
         gamma = 0.99
-        alpha = 0.99
-        #alpha = 2./(i+1)
-        for t in reversed(range(T-1)):
+        alpha = 0.02
+        #alpha = 1./((i+1)**2)
+        for t in reversed(range(0,T-1,3)):
             index = (t+1)*3
             state = episode[index - 3]
             action = episode[index - 2]
@@ -116,14 +119,16 @@ def learn(theta=None):
 
             R = gamma * R + reward
             policy, phi_values = get_policy(theta, state, True)
+            #print(phi_values[action])
             phi_s_a = phi_values[action]
-            gradient = phi_s_a
+            gradient = copy.deepcopy(phi_values[action])
 
             for j in  range(env.action_space.n):
                 phi_s_c = phi_values[j]
-                gradient -= policy[j] * phi_s_c
+                gradient -= policy[j] * phi_values[j]
 
             theta += alpha * np.power(gamma, t) * R * gradient
+            #print(theta)
 
     return theta
 
@@ -135,7 +140,7 @@ def run():
         inference(theta)
         theta = learn(theta)
         print(str(i))
-        print(starting_theta - theta)
+        print(theta)
 
 
 
